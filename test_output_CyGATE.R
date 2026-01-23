e29d84087e059b8bfe5e45dada438afd3da16c14
helenewegener1 <helene.wegener@gmail.com>
2026-01-23 12:16:46 +0100

    adding ./* to gzippness

--- test_output_CyGATE.R
+++ test_output_CyGATE.R
@@ -1,42 +1,81 @@
-
+library(readr)
 library(dplyr)
-library(glue)
 library(stringr)
+library(caret)   # for F1 calculation
 
-# Training data with label column
-test_y_path <- glue("/home/projects/dp_immunoth/people/helweg/projects/benchmarking/prep_data/out/test_y.zip")
-pred_y_path <- glue("/home/projects/dp_immunoth/people/helweg/projects/benchmarking/ob-pipeline-CyGATE/predicted.zip")
+# Set paths 
+test_y_path <- "/Users/srz223/Documents/courses/Benchmarking/repos/ob-pipeline-cytof/out/data/data_import/dataset_name-FR-FCM-Z2KP_virus_final_seed-42/preprocessing/data_preprocessing/num-1_test-sample-limit-5/data_import.test.labels.tar.gz"
+pred_y_path <- "/Users/srz223/Documents/courses/Benchmarking/repos/ob-pipeline-cytof/out/data/data_import/.0a2e7b8e58f3cc4a3859f604f164c7ff4a1398ac6dff16c26d45d97b876997c7/preprocessing/data_preprocessing/.7fa4750eefc52d3bd8051ca6a0cc5d9bde7c21938c0d8f384542306fe0569059/analysis/cygate/default/data_import_predicted_labels.tar.gz"
 
-# LOAD Y TEST
-test_y_zip_contents <- unzip(test_y_path, list = TRUE)$Name
-csv_files <- test_y_zip_contents[grepl("\\.csv$", test_y_zip_contents)]
+# Load data 
+test_y_files <- utils::untar(test_y_path, list = TRUE)
+test_y_list <- setNames(vector("list", length(test_y_files)), test_y_files)
+tmp <- tempdir()
+utils::untar(test_y_path, exdir = tmp)
 
-test_y_list <- lapply(csv_files, function(f) {
-  read.csv(unz(test_y_path, f))
-})
-
-# LOAD Y PRED
-pred_y_zip_contents <- unzip(pred_y_path, list = TRUE)$Name
-csv_files <- pred_y_zip_contents[grepl("\\.csv$", pred_y_zip_contents)]
-
-pred_y_list <- lapply(csv_files, function(f) {
-  read.csv(unz(pred_y_path, f))
-})
-
-# COMPARE
-
-
-# Test accuracy 
-for (sample in 1:length(test_y_list)){
-  
-  # sample <- 1
-
-  # Extract y test and pred for the current sample
-  test_y <- test_y_list[[sample]][, 1]
-  pred_y <- pred_y_list[[sample]][, 1]
-
-  # Accuracy
-  acc <- mean(pred_y == test_y)
-  print(acc)
-  
+for (file in test_y_files) {
+  df <- read_csv(file.path(tmp, file), col_names = FALSE)
+  test_y_list[[file]] <- df
 }
+names(test_y_list)
+names(test_y_list) <- names(test_y_list) %>% stringr::str_split_i("_", 2) %>% stringr::str_split_i("-", 3)
+names(test_y_list)
+
+pred_y_files <- utils::untar(pred_y_path, list = TRUE)
+pred_y_list <- setNames(vector("list", length(pred_y_files)), pred_y_files)
+tmp <- tempdir()
+utils::untar(pred_y_path, exdir = tmp)
+
+for (file in pred_y_files) {
+  df <- read_csv(file.path(tmp, file), col_names = FALSE)
+  pred_y_list[[file]] <- df
+}
+
+names(pred_y_list)
+names(pred_y_list) <- names(pred_y_list) %>% stringr::str_split_i("/", 8) %>% stringr::str_split_i("_", 3) %>% stringr::str_split_i("-", 3)
+names(pred_y_list)
+
+
+# F1 score 
+
+f1_results <- data.frame(
+  file = character(),
+  f1_macro = numeric(),
+  f1_micro = numeric(),
+  stringsAsFactors = FALSE
+)
+
+for (fname in names(test_y_list)) {
+  
+  # ensure match exists
+  if (!fname %in% names(pred_y_list)) {
+    warning(paste("No prediction file for:", fname))
+    next
+  }
+  
+  y_true <- test_y_list[[fname]]$X1 %>% factor()
+  y_pred <- pred_y_list[[fname]]$X1 %>% factor(levels = levels(y_true))
+  
+  levels(y_true)
+  levels(y_pred)
+  # caret::confusionMatrix gives precision/recall per class
+  cm <- caret::confusionMatrix(y_pred, y_true)
+  
+  # Macro F1 = average of class F1s
+  f1_per_class <- 2 * (cm$byClass[,"Sensitivity"] * cm$byClass[,"Pos Pred Value"]) /
+    (cm$byClass[,"Sensitivity"] + cm$byClass[,"Pos Pred Value"])
+  
+  f1_macro <- mean(f1_per_class, na.rm = TRUE)
+  
+  # Micro F1 = accuracy for multi-class
+  f1_micro <- cm$overall["Accuracy"]
+  
+  # Store results
+  f1_results <- rbind(
+    f1_results,
+    data.frame(file = fname, f1_macro = f1_macro, f1_micro = f1_micro)
+  )
+}
+
+print(f1_results)
+
